ad3f872af77a512074a1e887c83d5d857a43ce52
Nagy Bálint <balint.boy@gmail.com>
2022-10-08 21:51:33 +0200

    VS extension skeleton

---
+++ src/EntityFrameworkCoreScaffoldingVSExtension/Commands/OpenConfigCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace EntityFrameworkCoreScaffoldingVSExtension
+{
+    [Command(PackageIds.OpenConfigCommand)]
+    internal sealed class OpenConfigCommand : BaseCommand<OpenConfigCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            var view = new ScaffoldingSettingsView();
+            view.ShowModal();
+        }
+    }
+}
---
+++ src/EntityFrameworkCoreScaffoldingVSExtension/Commands/RunScaffoldingCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace EntityFrameworkCoreScaffoldingVSExtension
+{
+    [Command(PackageIds.RunScaffoldingCommand)]
+    internal sealed class RunScaffoldingCommand : BaseCommand<RunScaffoldingCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await VS.MessageBox.ShowWarningAsync("RunScaffoldingCommand", "Button clicked");
+        }
+    }
+}
